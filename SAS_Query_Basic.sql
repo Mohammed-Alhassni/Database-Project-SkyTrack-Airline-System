e803f24ce214285f0a8996e0d5b89c26a3449607
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2026-05-04 00:35:24 +0400

    Updated Basic sql

--- SAS_Query_Basic.sql
+++ SAS_Query_Basic.sql
@@ -1,10 +1,23 @@
 use SAS
 
 -- 1. List all flights and their current status, ordered by departure datetime from earliest to latest.
-
+select *
+from Flight
+order by depature_datetime
+--default is asc, smaller is early in case of time
 
 -- 2. Show all passengers, ordered alphabetically by full name. 
+select *
+from Passenger
+order by f_name, l_name
+-- sort first name then last
+-- SELECT * FROM Passenger ORDER BY f_name + ' ' + l_name; --SQL server
+-- SELECT * FROM Passenger ORDER BY f_name || ' ' || l_name; --SQL
+
 -- 3. List all aircraft and their seating capacity, ordered from largest to smallest. 
+select *
+from Aircraft
+order by seat_capacity desc
 -- 4. Show all bookings and their class. Display only distinct class values that exist in the system.
 -- 5. List all flights that have a status of 'Delayed' or 'Cancelled'.
 -- 6. Show all passengers whose nationality is 'Omani'. 
