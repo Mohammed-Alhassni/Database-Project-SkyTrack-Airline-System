42eab6f31c5e72eeb6ef25196679c9ace82223c2
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-05-03 17:06:26 +0400

    Updated, checkpoint

---
+++ SAS_Query_Basic.sql
@@ -0,0 +1,11 @@
+use SAS
+
+-- 1. List all flights and their current status, ordered by departure datetime from earliest to latest.
+
+
+-- 2. Show all passengers, ordered alphabetically by full name. 
+-- 3. List all aircraft and their seating capacity, ordered from largest to smallest. 
+-- 4. Show all bookings and their class. Display only distinct class values that exist in the system.
+-- 5. List all flights that have a status of 'Delayed' or 'Cancelled'.
+-- 6. Show all passengers whose nationality is 'Omani'. 
+-- 7. List all airports, ordered by country.
