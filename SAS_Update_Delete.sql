a983fdc9325db30708ce9b5714c68e7fd8a1ede1
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-05-03 15:44:09 +0400

    Completed all the updates tasks

--- SAS_Update_Delete.sql
+++ SAS_Update_Delete.sql
@@ -1,2 +1,17 @@
 use SAS 
 
+--Upate Tasks
+
+update Flight set status = 'Completed' where flight_num = 4
+
+update Flight set status = 'Cancelled' where flight_num = 5
+
+update Booking set price_paid = price_paid + price_paid * 0.1
+--or UPDATE Booking SET price_paid = price_paid * 1.1;
+
+update Passenger set phone =9999999 where national_id =1
+
+update Crew_member set role = 'Pilot' where license_number =2
+
+
+
