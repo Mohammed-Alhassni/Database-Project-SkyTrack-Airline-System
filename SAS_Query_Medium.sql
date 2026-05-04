f2b1c2d918a8e07fd2842c227e10c5f1c373d9ba
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-05-04 11:12:41 +0400

    Added sql for basic query

---
+++ SAS_Query_Medium.sql
@@ -0,0 +1,13 @@
+use SAS
+
+
+-- 1. For each flight, show the flight number, the name of the origin airport, and the name of the destination airport.
+-- 2. Show each booking along with the full name of the passenger who made it and the flight number it belongs to.
+-- 3. List all crew members assigned to flight 'SK101', showing their full name and role.
+-- 4. Show all completed flights along with the aircraft model used on each flight.
+-- 5. For each passenger, show their full name and the total number of bookings they have made. Order by booking count from highest to lowest.
+-- 6. Show the total revenue collected from each booking class.
+-- 7. Count how many flights each aircraft has been assigned to.
+-- 8. List all flights that have more than one booking.
+-- 9. Show the full details of all bookings — passenger name, flight number, origin airport, destination
+-- airport, class, and price paid.
