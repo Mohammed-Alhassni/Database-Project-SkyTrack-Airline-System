2f2dbc5e8e52a6e1cc6e4de4784d4d767bf5cec4
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-05-04 15:02:25 +0400

    Fixed some previous question

--- SAS_Query_Medium.sql
+++ SAS_Query_Medium.sql
@@ -32,13 +32,34 @@ where Flight.status = 'Completed'
 select * from Passenger
 select * from Booking
 select Passenger.national_id , Passenger.f_name + ' ' + Passenger.l_name as [full_name],count(Booking.national_id) as [total_bookings] from Passenger 
-inner join Booking on Passenger.national_id=Booking.national_id
-group by Passenger.f_name, Passenger.l_name, Passenger.national_id
+left join Booking on Passenger.national_id=Booking.national_id --left join, because you also want to include Passangers with no Bookings
+group by Passenger.f_name, Passenger.l_name, Passenger.national_id --items in select either warpped with agg fun or group by
 order by [total_bookings] desc 
 
 
 -- 6. Show the total revenue collected from each booking class.
+select * from Booking
+select Booking.class, sum(price_paid) as [total_revenue] from Booking 
+group by Booking.class
+
 -- 7. Count how many flights each aircraft has been assigned to.
+select * from Flight
+select * from Aircraft
+select Aircraft.model, Aircraft.manufacturer, count(Flight.reg_num) as [total_flights] from Aircraft
+left join Flight on Aircraft.reg_num = Flight.reg_num
+group by Aircraft.model, Aircraft.manufacturer
+order by total_flights desc
+
 -- 8. List all flights that have more than one booking.
--- 9. Show the full details of all bookings — passenger name, flight number, origin airport, destination
--- airport, class, and price paid.
+select * from Flight
+select * from Booking
+select * from Airport
+select Flight.flight_num, Airport1.name as [origin], Airport2.name as [destination], Flight.depature_datetime, Flight.arrival_datetime, Flight.status, count(Booking.flight_num) as [booking_count] from Flight
+inner join Booking on Flight.flight_num = Booking.flight_num
+inner join Airport Airport1 on Airport1.IATA = Flight.origin_airport
+inner join Airport Airport2 on Airport2.IATA = Flight.destination_airport
+Group by Flight.flight_num, Flight.depature_datetime, Flight.arrival_datetime, Flight.status, Airport1.name, Airport2.name 
+Having (count(Booking.flight_num) > 1)
+
+-- 9. Show the full details of all bookings — passenger name, flight number, origin airport, destination airport, class, and price paid.
+
