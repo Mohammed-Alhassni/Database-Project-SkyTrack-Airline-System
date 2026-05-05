121754c9ac45818621b0cd531e254dcca062a8cd
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-05-05 14:11:15 +0400

    Added Display commands

--- CR-001/SAS_Update_Create_Alter.sql
+++ CR-001/SAS_Update_Create_Alter.sql
@@ -65,3 +65,13 @@ alter table Flight
 		on delete set null -- because it can accept null
 					
 
+select * from Aircraft
+select * from Airport
+select * from Booking
+select * from Crew_member
+select * from Flight
+select * from Flight_crew
+select * from Airline
+select * from Gate
+select * from Delay_log
+select * from Baggage
