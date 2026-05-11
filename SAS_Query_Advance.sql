f8b312e37b5e47aea65bbff96a2d4e0c5c2925c3
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-05-11 09:11:59 +0400

    Solved to 5 in query advanced

--- SAS_Query_Advance.sql
+++ SAS_Query_Advance.sql
@@ -15,8 +15,22 @@ group by Passenger.f_name + ' ' + Passenger.l_name
 having count(Booking.booking_id) = 0
 
 -- 3. For each flight, show the flight number and the total revenue generated from its bookings. Show only flights where the total revenue exceeds 500. Order from highest to lowest.
+select Flight.flight_num, sum(Booking.price_paid) as [total_revenue] from Flight 
+left join Booking on Flight.flight_num = Booking.flight_num
+group by Flight.flight_num
+having sum(Booking.price_paid) > 500
+order by sum(Booking.price_paid) desc
+
 -- 4. Show each crew member's full name and the total number of flights they have been assigned to. Show only crew members assigned to more than one flight.
+select Crew_member.f_name + ' ' + Crew_member.l_name as [full_name], count(Flight.flight_num) as [total_flights] from Crew_member
+left join Flight_crew on Crew_member.license_number = Flight_crew.license_number
+left join Flight on Flight_crew.flight_num = Flight.flight_num
+group by Crew_member.f_name + ' ' + Crew_member.l_name
+having count(Flight.flight_num) > 1
+
 -- 5. Find the average booking price per flight. Show only flights where the average price is above the overall average price across all bookings.
+
+
 -- 6. Show the flight with the highest number of bookings. Display its flight number, origin, destination, and total bookings.
 -- 7. For each booking class, show the total revenue, the number of bookings, the average price, the highest price, and the lowest price.
 -- 8. List all passengers who booked a flight that is currently 'Cancelled'. Show the passenger name, flight number, and booking date.
